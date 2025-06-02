f5ec27eebc2feb617d397a513e1b40aed038bdcc
Sattva <74269253+Sattva-108@users.noreply.github.com>
2025-06-02 15:33:34 +0300

    new worldmaarea dbc parser
    cleanup extractor placeholder funcs

--- pfQuest-master/toolbox/create_pfquest_db.sql
+++
@@ -1,7 +0,0 @@
--- Create pfquest database and load DBC tables
-CREATE DATABASE IF NOT EXISTS pfquest;
-USE pfquest;
-
--- Grant permissions to acore user
-GRANT ALL PRIVILEGES ON pfquest.* TO 'acore'@'localhost';
-FLUSH PRIVILEGES;
